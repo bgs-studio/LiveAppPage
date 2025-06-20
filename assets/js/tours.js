b5101bf6ac198a3a9de6791238db1bd312fd1237
Khadim Hussain Firdosi <153745777+bgs-studio@users.noreply.github.com>
2025-06-20 16:20:56 +0500

    Update
    very massive update

--- assets/js/tours.js
+++ assets/js/tours.js
@@ -2,11 +2,9 @@ const tours = {
   "1": {
     title: "Katpana Lake 360 Toure",
     description: "Explore Katpana Lake in full 360°",
-    url: "Tours/1/"
+    image: "assets/img/Katpana.png",
+    url: "Tours/1",
+    tourId:1
   },
-  "2": {
-    title: "Modern Apartment",
-    description: "Take a tour through a modern, cozy apartment",
-    url: "https://example.com/apartment-tour"
-  }
+  
 };
